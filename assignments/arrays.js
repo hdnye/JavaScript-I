aef9170c85d9dcecb3edd19d61c10c9906bb53a4
Heather Nye <heatherdnye@gmail.com>
2019-09-09 18:11:00 -0500

    Javascript-I

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -78,17 +78,16 @@ let inventory = [
 const findId = (carId) => {
 
   for (let i = 0; i < inventory.length; i++) {
-     if(inventory[i].id === carId) { 
-       console.log(`Car ${i+1} is an ${inventory[i].car_year} ${inventory[i].car_make} ${inventory[i].car_model}`);
-     }
-  }
+    if(inventory[i].id === carId) { 
+      console.log(`Car ${i+1} is an ${inventory[i].car_year} ${inventory[i].car_make} ${inventory[i].car_model}`);
+    }
+ }
 }
 findId(33);
 
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-
 for (let i = 0; i <inventory.length; i++) {
 
   if(i === inventory.length -1) {
@@ -96,19 +95,23 @@ for (let i = 0; i <inventory.length; i++) {
   }
 }
 
-
-let lastCar = 0;
 console.log();
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
 let carModelsSorted = [];
+
+
 console.log();
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
+
+
+
+
 console.log();
 
 // ==== Challenge 5 ====
@@ -120,3 +123,7 @@ console.log();
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
 console.log();
+function newFunction() {
+  ;
+}
+
